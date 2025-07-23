fc30645fe49fa4d13117a07611976fa540a07ce4
Carlos Morales <carlosjose006@gmail.com>
2025-07-22 21:05:34 -0400

    Ejecisios de sintaxis basica (Variables y tipo de datos)

---
+++ python/basico/Ejecicios.py
@@ -0,0 +1,35 @@
+# Este es un comentario de una sola linea
+
+"""
+Este es un comentario de varias lineas
+Puedes escribir varias lineas de texto aqui
+
+"""
+
+def saludar():
+    print("Hello, how are you?")
+
+saludar()
+
+# Tanbien podemos hacer operaciones matematicas
+numero = 5 + 3
+print(" El resultado es: ",numero)
+
+# creacion de variables
+
+age = 30
+name = "Carlos Morales"
+
+print(f"My name is {name} and I am {age} years old.")
+
+# Tipos de datos
+
+#Enteros
+entero = 10
+#Flotantes
+flotante = 10.5
+#Cadenas de texto
+cadena = "Hola, Python!"
+#Booleanos
+booleano = True
+
